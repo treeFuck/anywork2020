7202f35fe678f8d824dc9f3a531959558719ba4b
treeFuck <1552660178@qq.com>
2020-09-09 17:32:07 +0800

    9.9 完善登录注册

--- src/components/Login/regCon.vue
+++ src/components/Login/regCon.vue
@@ -1,79 +1,29 @@
+
 <style scoped lang="scss">
   .regCon {
     width: 100%;
+    @import './share.scss';
 
-    .title {
-      font-size: 2em;
-      text-align: center;
-      color: #548cfe;
+    .studentId .logo {
+      background-image: url("../../assets/images/account@3x.png");
     }
 
-    .inputCon {
-      color: #495060;
-
-      .input {
-        display: flex;
-        align-items: center;
-        margin: 0.5em 0;
-        border-bottom: 1px solid #548cfe;
-
-        img {
-          height: 1.3em;
-        }
-
-        input {
-          width: 100%;
-          height: 3em;
-          line-height: 3em;
-          font-size: 13px;
-          border: none;
-          outline: none;
-          text-indent: 1em;
-          background: #fff;
-        }
-      }
-
-      .barcode {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-
-        .barcodeImg {
-          width: 48%;
-        }
-
-        .input {
-          width: 48%;
-        }
-      }
+    .email .logo {
+      background-image: url("../../assets/images/letter@3x.png");
     }
 
-    .btnCon {
-      display: flex;
-      justify-content: space-between;
-      margin-top: 1em;
-
-      div {
-        height: 2.5em;
-        width: 48%;
-        line-height: 2.5em;
-        text-align: center;
-        cursor: pointer;
-        border-radius: 5px;
-      }
-
-      .toLogin {
-        color: #495060;
-        border: 1px solid #dddee1;
-        background-color: #f7f7f7;
-      }
-
-      .sure {
-        color: #fff;
-        border: 1px solid #fff;
-        background-color: #7f3ffd;
-      }
+    .phone .logo {
+      background-image: url("../../assets/images/phone@3x.png");
     }
+
+    .password .logo {
+      background-image: url("../../assets/images/lock@3x.png");
+    }
+
+    .surePassword .logo {
+      background-image: url("../../assets/images/preview@1x.png");
+    }
+
   }
 </style>
 
@@ -81,23 +31,35 @@
   <div class="regCon">
     <div class="title">注册</div>
     <div class="inputCon">
-      <div class="input">
-        <img src="../../assets/images/account@3x.png"/>
+      <div class="input studentId">
+        <div class="logo"></div>
         <input type="text" v-model="studentId" placeholder="学号"/>
       </div>
-      <div class="input">
-        <img src="../../assets/images/lock@3x.png"/>
+      <div class="input email">
+        <div class="logo"></div>
+        <input type="text" v-model="email" placeholder="邮箱"/>
+      </div>
+      <div class="input phone">
+        <div class="logo"></div>
+        <input type="text" v-model="phone" placeholder="手机"/>
+      </div>
+      <div class="input password">
+        <div class="logo"></div>
         <input type="password" v-model="password" placeholder="密码"/>
+      </div>
+      <div class="input surePassword">
+        <div class="logo"></div>
+        <input type="password" v-model="surePassword" placeholder="确认密码"/>
       </div>
       <div class="barcode">
         <img class="barcodeImg" :src="barcodeURL" alt="">
         <div class="input">
-          <input type="password" v-model="valcode" placeholder="验证码"/>
+          <input type="text" v-model="valcode" placeholder="验证码"/>
         </div>
       </div>
     </div>
     <div class="btnCon">
-      <div class="toLogin" @click="toLogin">去登陆</div>
+      <div class="toLogin" @click="toLogin">去登录</div>
       <div class="sure" @click="sure">注册</div>
     </div>
   </div>
@@ -114,8 +76,13 @@
     data() {
       return {
         studentId: "",
+        email: "",
+        phone: "",
         password: "",
+        surePassword: "",
         valcode: "",
+        mark: 0, // 0 注册学生，1注册教师
+        regEmail: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/
       };
     },
     computed: {
@@ -124,31 +91,62 @@
       }
     },
     methods: {
+      // 切换到登录
       toLogin() {
         this.$emit('update:model', true)
       },
+      // 请求注册
       sure() {
-        // 请求登录
         let send = {
           password: this.password,
           studentId: this.studentId,
           valcode: this.valcode
         }
-      //   if(!this.valcode) {
-      //     this.$Message.warning("请输入验证码");
-      //     return;
-      //   }
-      //   loginApi.loginHTTP(send).then(res => {
-      //     if (res.data.state == 1) {
-      //       // 登录成功后，存储用户信息
-      //       this.$store.commit('addUserInfo', res.data.data);
-      //       // 跳转页面
-      //       this.$router.push({name: "index"});
-      //     } else {
-      //       this.$Message.warning(res.data.stateInfo)
-      //     }
-      //   })
-      }
+        if (!this.judeg()) {
+          return;
+        }
+        //   loginApi.loginHTTP(send).then(res => {
+        //     if (res.data.state == 1) {
+        //     } else {
+        //       this.$Message.warning(res.data.stateInfo)
+        //     }
+        //   })
+      },
+      // 校验输入
+      judeg() {
+        if (!this.studentId.trim()) {
+          this.$Message.warning("请输入学号");
+          return false;
+        }
+        if (!this.email.trim()) {
+          this.$Message.warning("请输入邮箱");
+          return false;
+        } else if (!this.regEmail.test(this.email)) {
+          this.$Message.warning("邮箱格式错误");
+          return false;
+        }
+        if (!this.phone.trim()) {
+          this.$Message.warning("请输入手机号");
+          return false;
+        }
+        if (!this.password.trim()) {
+          this.$Message.warning("请输入密码");
+          return false;
+        }
+        if (!this.surePassword.trim()) {
+          this.$Message.warning("请再次输入确认密码");
+          return false;
+        }
+        if (this.surePassword != this.password) {
+          this.$Message.warning("两次输入密码不一致");
+          return false;
+        }
+        if (!this.valcode.trim()) {
+          this.$Message.warning("请输入验证码");
+          return false;
+        }
+        return true;
+      },
     }
   };
 </script>
--- src/share/net/request.js
+++ src/share/net/request.js
@@ -15,9 +15,10 @@ const _Request = axios.create({
  */
 _Request.interceptors.request.use((config) => {
     // 当存在token的时候，将token加到请求头上面
-    if (localStorage.getItem("AuthorizationAdmin")) {
+    if (localStorage.getItem("Authorization")) {
         config.headers['Authorization'] = JSON.parse(localStorage.getItem("Authorization")).value;
     }
+    console.log(config.headers)
     return config
 }, error => {
     //403
@@ -29,8 +30,8 @@ _Request.interceptors.request.use((config) => {
  */
 _Request.interceptors.response.use((result) => {
     // 当没有前面的问题的时候，返回请求对象的数据
-    //拿取头部证书
-    if(!localStorage.getItem("Authorization")) {
+    // 登录请求的时候，拿取头部证书
+    if(result.config.url == '/user/login') {
         localStorage.setItem("Authorization", JSON.stringify({
             value: result.headers.authorization,
         }))
