2422f041619a61527269bcbd0c38d4db8213eb78
Yu Shengyuan <1749238278@qq.com>
2020-09-20 20:51:13 +0800

    注释修改

--- src/share/net/request.js
+++ src/share/net/request.js
@@ -27,7 +27,8 @@ _Request.interceptors.request.use((config) => {
  * 统一对返回的数据进行过滤
  */
 _Request.interceptors.response.use((result) => {
-    //用户未登录访问直接跳转登陆, //防止多个接口请求3001执行多次重定向报错
+    //用户未登录访问直接跳转登陆, 防止多个接口请求3001执行多次重定向报错
+    //假如除了登陆以外url上还有login的话那么这里逻辑就无了
     if (result.data.state == 3001 && !/login/.test(location.href)) {
         router.replace("/login")
     }
